937dbf21a8ac4f837689e2044b4fe0f5dd41da7b
dmitry <65540172+kalibdune@users.noreply.github.com>
2023-10-10 15:14:18 +0300

    Update bullsCows.cpp

--- stage5/bullsCows.cpp
+++ stage5/bullsCows.cpp
@@ -1,70 +1,74 @@
-#include <cstdlib>
 #include <iostream>
 #include <time.h>
+#include <string>
 
 using namespace std;
 
-bool isUniqe(int *arr, int num) {
-  for (int i = 0; i < 4; ++i) {
-    if (arr[i] == num) {
-      return false;
+bool isUniqe(int* arr, int num) {
+    for (int i = 0; i < 4; ++i) {
+        if (arr[i] == num) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
-int *getRandomNum() {
-  static int arr[4];
-  int i = 0;
-  while (i < 4) {
-    srand(time(0));
-    int num = rand() % 10;
+int* getRandomNum() {
+    static int arr[4];
+    int i = 0;
+    while (i < 4) {
+        srand(time(0));
+        int num = rand() % 10;
 
-    if (isUniqe(arr, num)) {
-      cout << num << "\n";
-      arr[i] = num;
-      ++i;
+        if (isUniqe(arr, num)) {
+            cout << num;
+            arr[i] = num;
+            ++i;
+        }
     }
-  }
 
-  return arr;
+    return arr;
 }
 
 int main() {
 
-  int num = rand() % 10;
-  int *arrNum = getRandomNum();
+    int* arrNum = getRandomNum();
 
-  cout << "just guess!\n";
+    cout << "\njust guess!\n";
 
-  while (true) {
-    int plus = 0;
-    int minus = 0;
+    while (true) {
+        int guess;
+        cin >> guess;
+        if (guess > 9999) {
+            cout << "num higher than 4 digits\n";
+            break;
+        }
+        int arrGuess[4];
 
-    int guess;
-    cin >> guess;
-    int arrGuess[4];
+        arrGuess[0] = guess / 1000;
+        arrGuess[1] = (guess % 1000) / 100;
+        arrGuess[2] = (guess % 100) / 10;
+        arrGuess[3] = guess % 10;
 
-    arrGuess[0] = guess / 1000;
-    arrGuess[1] = (guess % 1000) / 100;
-    arrGuess[2] = (guess % 100) / 10;
-    arrGuess[3] = guess % 10;
+        string state;
 
-    for (int i = 0; i < 4; ++i) {
-      if (arrGuess[i] == arrNum[i]) {
-        ++plus;
-      } else if (!isUniqe(arrNum, arrGuess[i])) {
-        ++minus;
-      } else {
-        NULL;
-      }
+        for (int i = 0; i < 4; ++i) {
+            if (arrGuess[i] == arrNum[i]) {
+                state += "+";
+            }
+            else if (!isUniqe(arrNum, arrGuess[i])) {
+                state += "-";
+            }
+            else {
+                state += "x";
+            }
+        }
+
+        if (state == "++++") {
+            cout << "Great!!\n";
+            break;
+        }
+
+        cout << state + "\n";
     }
-
-    if (plus == 4) {
-      cout << "Great!!\n";
-      break;
-    }
-
-    cout << "Pluses: " << plus << "\nMinuses: " << minus << "\n\n";
-  }
 }
