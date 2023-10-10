0546ca20d2d60c521872ef91ae80fdf2d5ef1c13
dmitry <65540172+kalibdune@users.noreply.github.com>
2023-10-10 15:15:42 +0300

    Update monk.cpp

--- stage5/monk.cpp
+++ stage5/monk.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int monks[600][3] = {{0}, {0}, {0}};
+int monks[600][3] = { {0}, {0}, {0} };
 
 int getMonkTree(int monk) {
     for (int i = 0; i < 600; ++i) {
         for (int j = 0; j < 3; ++j) {
             if (monks[i][j] == monk) {
-                return i+1;
+                return i + 1;
             }
         }
     }
@@ -20,18 +20,18 @@ int main() {
     cout << "enter number of data rows: ";
     cin >> numRows;
 
-    while(numRows != 0) {
+    while (numRows != 0) {
         int a, b, c, d;
         cin >> a >> b >> c >> d;
-        monks[a-1][0] = b;
-        monks[a-1][1] = c;
-        monks[a-1][2] = d;
+        monks[a - 1][0] = b;
+        monks[a - 1][1] = c;
+        monks[a - 1][2] = d;
         --numRows;
     }
 
     cout << "enter type of task and monk number\n";
 
-    while(true) {
+    while (true) {
         int taskNum, num1, num2;
         cin >> taskNum;
         if (taskNum == 1) {
@@ -39,8 +39,8 @@ int main() {
             int tmp = getMonkTree(num1);
             if (tmp == 0) {
                 cout << num1 << " is not a monk!\n";
-
-            } else {
+            }
+            else {
                 cout << tmp << " ";
                 while (tmp != 1) {
                     tmp = getMonkTree(tmp);
@@ -48,9 +48,39 @@ int main() {
                 }
                 cout << "\n";
             }
-        } else {
+        }
+        else {
             cin >> num1 >> num2;
+            int tmp1 = getMonkTree(num1);
+            int tmp2 = getMonkTree(num2);
+            if (tmp1 == 0) {
+                cout << num1 << " is not a monk!\n";
 
+            }
+            else if (tmp2 == 0) {
+                cout << num2 << " is not a monk!\n";
+            }
+            else {
+                int arr1[600];
+                int arr2[600];
+                int s = 0;
+                while (tmp1 != 0 && tmp2 != 0) {
+                    cout << getMonkTree(tmp1) << "\t" << s << "\n";
+                    tmp1 = getMonkTree(tmp1);
+                    tmp2 = getMonkTree(tmp2);
+                    arr1[s] = tmp1;
+                    arr2[s] = tmp2;
+                    ++s;
+                }
+
+                for (int i = 0; i < s; ++i) {
+                    for (int j = 0; j < s; ++j) {
+                        if (arr1[i] == arr2[j]) {
+                            cout << num1 << " and " << num2 << " monks. Teacher: " << arr1[i] << "\n";
+                        }
+                    }
+                }
+            }
         }
     }
 }
